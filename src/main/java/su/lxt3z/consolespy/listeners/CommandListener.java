d890e487b3fdbaa44b022eaf70be9d84ba1089ea
lxt3z <124895263+lxt3z@users.noreply.github.com>
2025-08-12 00:45:04 +0300

    added logManager and the structure has been changed

--- src/main/java/su/lxt3z/consolespy/listeners/CommandListener.java
+++ src/main/java/su/lxt3z/consolespy/listeners/CommandListener.java
@@ -8,36 +8,45 @@ import su.lxt3z.consolespy.Main;
 
 public class CommandListener implements Listener {
     private final Main plugin;
+    private final PlayerData playerData;
+    private final MessageManager messageManager;
+    private final LogManager logManager;
 
     public CommandListener(Main plugin) {
         this.plugin = plugin;
+        this.playerData = Main.getPlayerData();
+        this.messageManager = plugin.getMessageManager();
+        this.logManager = new LogManager(plugin);
     }
 
     @EventHandler
-    public void onCommand(PlayerCommandPreprocessEvent event) {
+    public void onPlayerCommandPreprocess(PlayerCommandPreprocessEvent event) {
         Player player = event.getPlayer();
-        if (Main.getPlayerData().isHidden(player.getName())) {
+        String command = event.getMessage().substring(1);
+        String playerName = player.getName();
+
+        if (plugin.getConfig().getStringList("ignore-commands").contains(command.split(" ")[0].toLowerCase())) {
             return;
         }
 
-        String command = event.getMessage().split(" ")[0];
-        if (plugin.getConfig().getStringList("ignore-commands").contains(command)) {
+        if (playerData.isIgnored(playerName)) {
             return;
         }
 
-        String message = plugin.getMessageManager().getMessage("info-message")
-                .replace("%player%", player.getName())
-                .replace("%command%", event.getMessage());
+        if (playerData.isHiding(playerName)) {
+            return;
+        }
 
-        for (String spyName : Main.getPlayerData().getSpys()) {
-            if (spyName.equals(player.getName()) || Main.getPlayerData().isIgnored(spyName, player.getName())) {
-                continue;
-            }
-
-            Player spy = plugin.getServer().getPlayer(spyName);
-            if (spy != null) {
-                spy.sendMessage(message);
+        for (String spy : playerData.getSpies()) {
+            Player spyPlayer = plugin.getServer().getPlayer(spy);
+            if (spyPlayer != null && spyPlayer.isOnline()) {
+                spyPlayer.sendMessage(messageManager.getMessage("info-message")
+                        .replace("%player%", playerName)
+                        .replace("%command%", command));
             }
         }
+
+        logManager.logCommand(playerName, command); // added logManager
     }
+    // soon new function
 }
